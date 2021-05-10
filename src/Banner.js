c3a364bd516e136ec6048cc3801c95d929f03bc7
Mateusz Lewartowski <lewar2013c@gmail.com>
2021-05-10 18:04:28 +0200

    added movieDB with APi to get banner and row component with request of title,image etc. && added style for Row,Banner component

--- src/Banner.js
+++ src/Banner.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 import './Banner.css';
-import banner from './Images/banner.jpg'
+import requests from './Request';
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+        Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, [])
 
   function textcute(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + '...' : string;
@@ -12,19 +27,20 @@ function Banner() {
     <header className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url(${banner})`,
-        backgroundPosition: "center bottom"
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center"
       }}>
       <div className="banner__contents">
         <h1 className="banner__title">
-          Movie Name
+          {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My list</button>
         </div>
         <h1 className="banner__description">
-          {textcute(`Text Desc`, 150)}
+          {textcute(
+            movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--fadeBottom" />
