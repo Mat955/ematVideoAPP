04d3d1432f75c5ddc19e28d53f421ec006b6d81d
Mateusz Lewartowski <lewar2013c@gmail.com>
2021-05-10 12:58:11 +0200

    project init react

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCByIa39v9--Rm42Ajigs-oLOCvvoENkh0",
+  authDomain: "emat-videoapp.firebaseapp.com",
+  projectId: "emat-videoapp",
+  storageBucket: "emat-videoapp.appspot.com",
+  messagingSenderId: "949903960467",
+  appId: "1:949903960467:web:b9923f557cd3bb4baaa066"
+};
