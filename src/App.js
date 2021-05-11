a9c2cd288193d0c01d9b2047fc1042c39c793ce4
Mateusz Lewartowski <lewar2013c@gmail.com>
2021-05-11 14:29:35 +0200

    added redux function to login & loginout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import Login from './screens/LoginScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './userSlice';
 
 function App() {
 
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          }))
+      } else {
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../firebase';
 import './SignupScreen.css';
 
 function SignupScreen() {
 
-  const register = (e) => {
+  const emailRef = useRef(null);
+  const passwordref = useRef(null);
+
+  const register = e => {
     e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordref.current.value
+      )
+      .then((authUser) => {
+
+      })
+      .catch(error => {
+        alert(error.message)
+      });
   };
 
   const signIn = e => {
     e.preventDefault();
-  }
+
+    auth.signInWithEmailAndPassword(
+      emailRef.current.value,
+      passwordref.current.value
+    )
+      .then(authUser => {
+
+      })
+      .catch(error => alert(error.message));
+  };
 
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign in</h1>
-        <input placeholder="Email" typeof="email" />
-        <input placeholder="Password" type="password" />
+        <input ref={emailRef} placeholder="Email" typeof="email" />
+        <input ref={passwordref} placeholder="Password" type="password" />
         <button type="submit" onClick={signIn}>Sign In</button>
         <h4>
           <span className="signupScreen__gray">New to ematPRO Films? </span>
